e2b74f20eabe2dad56029ac6c74e12c670e277c0
henriquewallace <henrique.wallace@townsq.com.br>
2023-10-31 00:06:00 -0300

    create CategoryMapper interface

---
+++ src/main/java/com/jeff/springfruitshop/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.jeff.springfruitshop.api.v1.mapper;
+
+import com.jeff.springfruitshop.api.v1.model.CategoryDTO;
+import com.jeff.springfruitshop.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
